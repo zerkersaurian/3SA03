a9ad73ff750dc31f2200b96c77ba1a05f242e9b4
thanat hirunyathon <6110110184e@psu.ac.th>
2020-08-14 22:16:58 +0700

    4th commit: add gray border

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import WordCard from './WordCard';
 
 function App() {
  return (
-  <div>
+  <div id="main" className="bg">
+    <div id="border0" className="border"> </div>
     <div id="header" className="App-header">Word sorting game</div>
-    <div id="instruction" className="App-instruct"> How to play? : spell "HELLO" word by clicking character card from this puzzle</div>
-    <WordCard value="hello"/>
+    <div id="border" className="border"> </div>
+    <div id="instruction" className="App-instruct"> How to play? : spell "computer-engineer" word by clicking character card from this puzzle</div>
+    <div id="border2" className="border"> </div>
+    <WordCard value="computer-engineer"/>
+    <div id="border3" className="border"> </div>
   </div>
  );
 }
