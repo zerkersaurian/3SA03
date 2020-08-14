29cd874c2fbdf9d509f0dca8f047b20b7245bd26
thanat hirunyathon <6110110184e@psu.ac.th>
2020-08-14 15:24:05 +0700

    first?

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import WordCard from './WordCard';
+function App() {
+ return (
+  <div>
+    <WordCard value="hello"/>
+  </div>
+ );
+}
+export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+   let word = given_word.toUpperCase()
+   let chars = _.shuffle(Array.from(word))
+   return {
+      word,
+      chars,
+      attempt: 1,
+      guess: '',
+      completed: false
+   }
+  }
+export default function WordCard(props){
+
+   const [state, setState] = useState(prepareStateFromWord(props.value))
+   const activationHandler = (c) => {
+      console.log(`${c} has been activated.`)
+
+      let guess = state.guess + c
+      setState({...state, guess})
+
+      if(guess.length == state.word.length){
+         if(guess == state.word){
+         console.log('yeah!')
+         setState({...state, completed: true})
+         }
+         else{
+         console.log('reset, next attempt')
+         setState({...state, guess: '', attempt: state.attempt + 1})
+         }
+      }
+      console.log(guess)
+   }     
+   
+   return (
+   <div>
+      { 
+         state.chars.map((c,i) => 
+            <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
+         )
+      }
+   </div>
+   )
+}
