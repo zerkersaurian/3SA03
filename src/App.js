40bea556d360ffde96da7b654d12d5a1efeb7bb0
thanat hirunyathon <6110110184e@psu.ac.th>
2020-08-14 21:54:43 +0700

    3rd commit: adding header and add styles to App.css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import WordCard from './WordCard';
 
 function App() {
  return (
-  <div>{
+  <div>
+    <div id="header" className="App-header">Word sorting game</div>
+    <div id="instruction" className="App-instruct"> How to play? : spell "HELLO" word by clicking character card from this puzzle</div>
     <WordCard value="hello"/>
-  }
   </div>
  );
 }
